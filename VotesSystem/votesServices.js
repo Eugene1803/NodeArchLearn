3d991a398e8fb4382b86df51c0895844a55dda9e
Olga Pelevaniuk <oiapelevaniuk@gmail.com>
2020-03-08 15:13:10 +0300

    ergerg
    Signed-off-by: Olga Pelevaniuk <oiapelevaniuk@gmail.com>

--- VotesSystem/votesServices.js
+++ VotesSystem/votesServices.js
@@ -10,8 +10,8 @@ webserver.use(express.json());
 //webserver.use(express.urlencoded({extended:true}));
 
 const initVoteOptions = [
-    {name: 'белый', value: 0, votes: 0},
-    {name: 'черный', value: 1, votes: 0},
+    {name: 'белый', value: 0},
+    {name: 'черный', value: 1},
 ];
 
 webserver.get('/index.html', (req, res) => { 
@@ -21,40 +21,39 @@ webserver.get('/index.html', (req, res) => {
 webserver.get('/variants', (req, res) =>
     res.send(initVoteOptions))
 
+    webserver.post('/vote', (req, res) => {
+
+       const data =  fs.readFileSync(path.join(__dirname, 'votesResults.json'));
+            let currentStatistics;
+            
+            if (!data) {
+                currentStatistics = initVoteOptions.map(item => ({value: item.value, votes: 0}));
+               
+            }else {
+                currentStatistics = JSON.parse(data);  
+            }
+
+            const newStatistics = currentStatistics.map(item =>{ 
+                if(+item.value === +req.body.value) item.votes++;
+                return item;
+             })
+        fs.writeFileSync(path.join(__dirname, 'votesResults.json'),JSON.stringify(newStatistics));
+        
+          res.send(true);
+
+        
+    })
+
 webserver.get('/stat', (req, res) => {
     
-    fs.readFile(path.join(__dirname, 'votesResults.json'), data => {
-    
-        if (!data) {res.send(initVoteOptions)}else {
-            res.send(data);
-        }
-    });
+    const data = fs.readFileSync(path.join(__dirname, 'votesResults.json'));
+    if (!data) {
+        res.send(initVoteOptions.map(item => ({value: item.value, votes: 0})))
+    }else {
+        res.send(data);
+    }
 })
-
-webserver.post('/vote', (req, res) => {
-
-    fs.readFile(path.join(__dirname, 'votesResults.json'), data => {
-        let currentStatistics;
-        if (!data) {
-            currentStatistics = [...initVoteOptions];
-           
-        }else {
-            currentStatistics = JSON.parse(data);
-            
-        }
-        const newStatistics = currentStatistics.map(item =>{ 
-            if(+item.value === +req.body.value) item.votes++;
-            return item;
-         })
-    fs.writeFileSync(path.join(__dirname, 'votesResults.json'),JSON.stringify(newStatistics));
-    
-      res.send(JSON.stringify(newStatistics));
-    });
-    
-})
-
 
 webserver.listen(port,()=>{ 
     console.log("web server running on port "+port);
 }); 
- 
